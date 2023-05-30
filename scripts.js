88c1f904f46957d0bc5ae379bf62e74b236d3513
PedrohqRibeiro <thepedrinhoguns@hotmail.com>
2023-05-29 21:07:31 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,71 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real
+    const currencyValueToConverted = document.querySelector(".currency-value") // moedas convertidas no caso agr o dolar 
+
+
+ 
+
+
+    const dolarToday = 5.20
+    const euroToday = 6.20
+
+
+    if (currencySelect.value == "dolar") {//Se o select estiver selecionado o valor dolar, entre aqui
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-Us", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+
+    }
+
+    if (currencySelect.value == "euro") { //se o select estiver selecionado o valor euro, entre aqui
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE",{
+            style:"currency",
+            currency:"EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+
+    }
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+}
+
+
+function changecurrency(){
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+    
+    if(currencySelect.value == "dolar"){
+
+        currencyName.innerHTML = "Dólar americano"
+        currencyImage.src="./assets/dolar.png"
+    }
+
+    if(currencySelect.value == "euro"){
+        currencyName.innerHTML ="Euro"
+        currencyImage.src="./assets/euro.png"
+        
+    }
+
+    convertValues()
+    
+    
+    
+   
+
+
+
+
+
+
+}
+
+currencySelect.addEventListener("change", changecurrency)
+convertButton.addEventListener("click", convertValues)
